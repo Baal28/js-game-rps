89425c20ecc9da6b75f562642adbb43ef931836a
Baal28 <107602112+Baal28@users.noreply.github.com>
2025-04-28 14:44:54 -0400

    modified the function for computer choice. added a function for human choice. added a function that evaluates both choices.

--- Js/script.js
+++ Js/script.js
@@ -1,16 +1,60 @@
+let humanScore = 0;
+let computerScore = 0;
+const ROCK = 'rock';
+const PAPER = 'paper';
+const SCISSORS = 'scissors';
+
 function getComputerChoice() {
-    const ROCK = 'rock';
-    const PAPER = 'paper';
-    const SCISSORS = 'scissors'
+    //const ROCK = 'rock';
+    //const PAPER = 'paper';
+    //const SCISSORS = 'scissors';
     let choice = Math.floor(Math.random() * 3);
 
     if (choice === 0) {
-        return console.log(ROCK);
+        return  ROCK;
     } else if (choice === 1) {
-        return console.log(PAPER);
+        return PAPER;
     } else {
-        return console.log(SCISSORS);
+        return SCISSORS;
     }
 }
 
-getComputerChoice();
+//getComputerChoice();
+
+function getHumanChoice() {
+    //const ROCK = 'rock';
+    //const PAPER = 'paper';
+    //const SCISSORS = 'scissors';
+    let hChoice = prompt('Insert your choice Rock, Paper, Scissors ?').toLowerCase();
+
+    if (hChoice === ROCK) {
+        return ROCK;
+    } else if (hChoice === PAPER) {
+        return PAPER;
+    } else if (hChoice === SCISSORS) {
+        return SCISSORS;
+    }
+
+}
+
+// getHumanChoice();
+
+const computerSelection = getComputerChoice();
+const humanSelection = getHumanChoice();
+
+function playRound(humanChoice, computerChoice) {
+    if ( humanChoice === ROCK && computerChoice === SCISSORS || humanChoice === PAPER && computerChoice === ROCK || humanChoice === SCISSORS && computerChoice ===  PAPER) {
+        humanScore++
+        return alert(`You Win!, ${humanChoice} beats ${computerChoice} Your score is: ${humanScore}` );
+        } else if (humanChoice === ROCK && computerChoice === ROCK || humanChoice === PAPER && computerChoice === PAPER || humanChoice === SCISSORS && computerChoice ===  SCISSORS) {
+            alert('It\'s a draw! Try again! ');
+        } else {
+        computerScore++    
+        return alert(`You Lose! ${computerChoice} beats ${humanChoice} The computer score is: ${computerScore}`)
+    }
+}
+
+
+//console.log(computerSelection);
+
+playRound(humanSelection, computerSelection );
